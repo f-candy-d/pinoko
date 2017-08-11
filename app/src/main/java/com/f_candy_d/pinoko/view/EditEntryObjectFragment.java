0d340919ee542622e96aa2c772dc1d47c6d8c878
f-candy-d <pagunouetamuramaro@gmail.com>
2017-08-11 11:26:05 +0900

    add new

---
+++ app/src/main/java/com/f_candy_d/pinoko/view/EditEntryObjectFragment.java
@@ -0,0 +1,47 @@
+package com.f_candy_d.pinoko.view;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+import com.f_candy_d.pinoko.model.EntryObject;
+
+/**
+ * Created by daichi on 8/11/17.
+ */
+
+public class EditEntryObjectFragment<T extends EntryObject> extends Fragment {
+
+    public interface MessageListener<T extends EntryObject> {
+        void onFinishEditing(final T content, final boolean isCanceled);
+    }
+
+    protected static final String ARG_CONTENT = "content";
+
+    private MessageListener<T> mMessageListener = null;
+    private T mContent;
+
+    public EditEntryObjectFragment() {}
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof EditEntryObjectFragment.MessageListener) {
+            mMessageListener = (EditEntryObjectFragment.MessageListener<T>) context;
+        } else {
+            throw new RuntimeException(context.toString()
+            + " must implement EditCourseTimeBlockFragment.MessageListener");
+        }
+    }
+
+    protected T getContent() {
+        return mContent;
+    }
+
+    protected void setContent(T content) {
+        mContent = content;
+    }
+
+    protected MessageListener<T> getMessageListener() {
+        return mMessageListener;
+    }
+}
